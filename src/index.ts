c1cddee21574185f029eeded207abe77cda1e669
Toño <joseantonio@z1.digital>
2024-09-11 18:32:45 +0200

    feat: add fastify apollo integration

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,56 @@
 import 'reflect-metadata'
-import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import Fastify from 'fastify'
+import { ApolloServer, BaseContext } from '@apollo/server'
+import {
+  fastifyApolloHandler,
+  fastifyApolloDrainPlugin,
+  ApolloFastifyContextFunction,
+} from '@as-integrations/fastify'
 import { PrismaClient } from '@prisma/client'
 import categoryResolvers from '@modules/categories/resolvers'
 import { buildSchema } from 'type-graphql'
 
 async function main() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
   const prisma = new PrismaClient()
+
+  interface ApolloContext extends BaseContext {
+    prisma: PrismaClient
+  }
+  const apolloContextFn: ApolloFastifyContextFunction<
+    ApolloContext
+  > = async () => ({
+    prisma,
+  })
 
   const schema = await buildSchema({
     resolvers: [...categoryResolvers],
     validate: false,
   })
+  const apollo = new ApolloServer<BaseContext>({
+    schema,
+    plugins: [fastifyApolloDrainPlugin(fastify)],
+  })
+  await apollo.start()
 
-  const server = new ApolloServer({ schema })
-
-  const { url } = await startStandaloneServer(server, {
-    context: async ({ req }) => ({ prisma }),
-    listen: { port: 4000 },
+  fastify.route({
+    url: '/graphql',
+    method: ['GET', 'POST', 'OPTIONS'],
+    // method: ['POST', 'OPTIONS''], // allow only POST and OPTIONS in prod
+    handler: fastifyApolloHandler(apollo, { context: apolloContextFn }),
   })
 
-  console.log(`✨🚀 Server ready at: ${url}`)
+  try {
+    await fastify.listen({ port: 3000 })
+    console.log(`✨🚀 Fastify server ready at: http://localhost:3000/`)
+    console.log(`✨🚀 GraphQL ready at: http://localhost:3000/graphql`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
 
-main().catch(console.error)
+void main()
