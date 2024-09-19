1c40d95365d114e4f4e79fa369c854679007ef31
Toño <joseantonio@z1.digital>
2024-09-19 09:42:33 +0200

    feat: update `FieldResolver` example

--- src/modules/categories/graphql/fields.ts
+++ src/modules/categories/graphql/fields.ts
@@ -1,11 +1,11 @@
 import { Category } from '@generated/type-graphql/models/Category.js'
-import {Resolver, FieldResolver} from 'type-graphql'
+import {Resolver, FieldResolver, Root} from 'type-graphql'
 
 @Resolver(() => Category)
 export class CategoryFieldResolver {
-  @FieldResolver(() => String, { nullable: true })
-  async fullName(): Promise<string | undefined> {
-    return 'hello'
+   @FieldResolver(() => String, { nullable: false })
+  async fullName(@Root() category: Category): Promise<string> {
+    return `${category.code}-${category.name}`
   }
 
   @FieldResolver(() => Date, { nullable: true })
