fa7928c0a57ede09047a1edf5fd9a19b9121ec2e
juanjosegb <juanjose.garciabeza@hotmail.com>
2024-09-25 14:57:59 +0200

    feat: tests

---
+++ src/modules/categories/__tests__/queries.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client'
+import { ApolloServer } from '@apollo/server'
+import 'reflect-metadata'
+import { buildSchema } from 'type-graphql'
+import categoryResolvers from '@modules/categories/resolvers.js'
+import authResolvers from '@modules/auth/resolvers.js'
+import { CustomAuthChecker } from '@core/auth.js'
+import assert from 'node:assert'
+
+const prisma = new PrismaClient()
+
+const contextValue = { prisma };
+
+async function getServer() {
+  const schema = await buildSchema({
+    resolvers: [...categoryResolvers, ...authResolvers],
+    validate: false,
+    authChecker: CustomAuthChecker,
+  })
+
+  return new ApolloServer({
+    schema
+  });
+}
+
+async function categories() {
+  const testServer = await getServer()
+  return testServer.executeOperation({
+      query: `
+            query categories {
+                categories {
+                    id
+                    name
+                    code
+                }
+            }
+        `,
+      variables: {}
+    },
+    { contextValue });
+}
+
+describe('categories', () => {
+  prisma.category.create({
+    data: {
+      name: "test",
+      code: "test",
+    },
+  })
+
+  describe('Categories list', () => {
+    it('return all categories', async () => {
+      const response = await categories();
+      assert(response.body.kind === 'single');
+      expect(response.body.singleResult.errors).toBeUndefined();
+      expect(response.body.singleResult.data?.categories).toHaveLength(1);
+    });
+  });
+});
